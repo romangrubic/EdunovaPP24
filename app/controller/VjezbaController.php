0bc379754bc000c4eda6b63ab4f12a49c87a545f
Roman Grubic <grubic.roman@gmail.com>
2022-02-09 22:51:50 +0100

    Vjezbanje poziva view-a iz kontrolera.

--- app/controller/VjezbaController.php
+++ app/controller/VjezbaController.php
@@ -6,4 +6,25 @@ class VjezbaController
     {
         echo 'Edunova';
     }
+
+    public function primjer2()
+    {
+        $sb = rand(2,9);
+        $ime='Edunova';
+        $o = new stdClass();
+        $o->ime='Pero';
+        $o->prezime='Perić';
+        $niz=[
+            'Osijek', 'Zagreb', 'Donji Miholjac'
+        ];
+        shuffle($niz);
+
+        $view = new View();
+        $view->render('ispisParametara',[
+            'slucajniBroj'=>$sb,
+            'skola'=>$ime,
+            'voditelj'=>$o,
+            'gradovi'=>$niz
+        ]);
+    }
 }
