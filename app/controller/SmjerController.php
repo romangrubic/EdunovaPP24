3b593e6f27ef8366e2beabd9dae568a8b319ba7d
Roman Grubic <grubic.roman@gmail.com>
2022-02-28 20:48:16 +0100

    Formatiranje broja

--- app/controller/SmjerController.php
+++ app/controller/SmjerController.php
@@ -5,10 +5,25 @@ class SmjerController extends AutorizacijaController
     // Putanja do phtml datoteke
     private $viewDir = 'privatno' . DIRECTORY_SEPARATOR . 'smjer' . DIRECTORY_SEPARATOR;
 
+    private $nf;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->nf = new \NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
+        $this->nf->setPattern('#,##0.00 kn');
+    }
+
     public function index()
     {
+        $smjerovi = Smjer::read();
+
+        foreach($smjerovi as $smjer){
+            $smjer->cijena=$this->nf->format($smjer->cijena);
+        }
+
         $this->view->render($this->viewDir . 'index',[
-            'smjerovi'=>Smjer::read(),
+            'smjerovi'=>$smjerovi,
             'css'=> '<link rel="stylesheet" href="' . App::config('url') . 'public/css/smjerindex.css">'
         ]);
     }
