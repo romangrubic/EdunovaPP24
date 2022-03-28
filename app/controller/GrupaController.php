d45f15cd35ae6d92f322258ea3a8a394e1aee06e
Roman Grubic <grubic.roman@gmail.com>
2022-03-28 20:44:55 +0200

    Kreiranje modela i kontrolera za Grupa

--- app/controller/GrupaController.php
+++ app/controller/GrupaController.php
@@ -4,8 +4,53 @@ class GrupaController extends AutorizacijaController
 {
     private $viewDir = 'privatno' . DIRECTORY_SEPARATOR . 'grupa' . DIRECTORY_SEPARATOR;
 
+    private $poruka;
+    private $entitet;
+
     public function index()
     {
-        $this->view->render($this->viewDir . 'index');
+        $this->view->render($this->viewDir . 'index',[
+            'entiteti' => Grupa::read()
+           ]);
+    }
+
+    public function novi()
+    {
+        header('location:' . App::config('url').'grupa/promjena/' . 
+        Grupa::create([
+            'naziv'=>'Nova grupa',
+            'smjer'=>Smjer::read()[0]->sifra,
+            'predavac'=>null,
+            'datumpocetka'=>null]
+            )
+        );
+    }
+
+    public function odustani($sifra)
+    {
+        if(Grupa::odustajanje($sifra)){
+            Grupa::delete($sifra);
+        }
+        header('location:' . App::config('url').'grupa/index');
+
+    }
+
+
+    public function promjena($sifra)
+    {
+        $this->entitet = Grupa::readOne($sifra);
+        $this->view->render($this->viewDir . 'promjena',[
+            'poruka'=>'Promjenite podatke',
+            'e'=>$this->entitet,
+            'smjerovi'=>Smjer::read(),
+            'predavaci'=>Predavac::read()
+        ]);
+    }
+
+
+    public function brisanje($sifra)
+    {
+        Grupa::delete($sifra);
+        header('location:' . App::config('url').'grupa/index');
     }
 }
---
+++ app/model/Grupa.php
@@ -0,0 +1,107 @@
+<?php
+
+class Grupa
+{
+
+    public static function odustajanje($kljuc)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            select count(*) from grupa 
+            where sifra=:parametar
+            and naziv=\'Nova grupa\'
+            and predavac is null
+            and datumpocetka is null
+            and smjer=(select sifra from smjer order by certificiran desc, naziv limit 1);
+        
+        '); 
+        $izraz->execute(['parametar'=>$kljuc]);
+        return $izraz->fetchColumn()==1 ? true : false;
+    }
+
+
+    public static function readOne($kljuc)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            select * from grupa where sifra=:parametar;
+        
+        '); 
+        $izraz->execute(['parametar'=>$kljuc]);
+        return $izraz->fetch();
+    }
+
+    // CRUD
+
+    //R - Read
+    public static function read()
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            select a.sifra, a.naziv, b.naziv as smjer, 
+            concat(d.ime, \' \', d.prezime) as predavac,
+            a.datumpocetka, count(e.polaznik) as polaznika
+            from grupa a inner join smjer b on 
+            a.smjer=b.sifra
+            left join predavac c on a.predavac =c.sifra 
+            left join osoba d on c.osoba =d.sifra
+            left join clan e on a.sifra=e.grupa 
+            group by a.sifra, a.naziv, b.naziv, 
+            concat(d.ime, \' \', d.prezime), a.datumpocetka;
+        
+        
+        '); 
+        $izraz->execute();
+        return $izraz->fetchAll();
+    }
+
+    //C - Create
+    // $parametri su asocijativni niz - tako mi odgovara
+    public static function create($parametri)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            insert into grupa (naziv,smjer,predavac,datumpocetka)
+            values (:naziv,:smjer,:predavac,:datumpocetka);
+        
+        '); 
+        $izraz->execute($parametri);
+        return $veza->lastInsertId();
+    }
+
+
+    //U - Update
+    public static function update($parametri)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            update grupa set 
+                naziv=:naziv,
+                smjer=:smjer,
+                predavac=:predavac,
+                datumpocetka=:datumpocetka
+                where sifra=:sifra;
+        
+        '); 
+        $izraz->execute($parametri);
+
+    }
+
+    //D - Delete
+    public static function delete($sifra)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            delete from grupa where sifra=:sifra;
+        
+        '); 
+        $izraz->execute(['sifra'=>$sifra]);
+
+    }
+} 
